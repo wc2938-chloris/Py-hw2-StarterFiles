9b3bb1635e8bc1f7f6fdab84f0a21bada7368dda
wc2938-chloris <wc2938@columbia.edu>
2026-02-27 01:04:43 -0500

    try update

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+    
+    return()
+
